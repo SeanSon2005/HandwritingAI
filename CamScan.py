da0be9794d7b1d5f195289b30c9f3ea6c49b983c
Sean Son <40509013+SeanSon2005@users.noreply.github.com>
2022-05-20 08:10:56 -0700

    commit so evan can do work

--- CamScan.py
+++ CamScan.py
@@ -1,48 +1,24 @@
-#import tensorflow as tf
-#from tensorflow import keras
-from distutils.command.clean import clean
-from PIL import Image, ImageEnhance
+from grpc import dynamic_ssl_server_credentials
 import numpy as np
 import cv2
-import os
 import re
-import math
+from matplotlib import pyplot as plt
 
-def annotate_image(image):
-    print("Generating Legibility Report")
-    scale_percent =  100# percent of original size
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+#
 
-    cropped = transformation(resized)
-    width = cropped.shape[1]
-    height = cropped.shape[0]
-    cropped = cropped[5:(height-5)]
+# ## **Use Gaussian Blurring combined with Adaptive Threshold**
 
-    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-    gray = BrightnessContrast(3,gray)
-    ret,thresh = cv2.threshold(gray,165,255,cv2.THRESH_BINARY)
-    kernel = np.ones((1, 1), np.uint8)
-    erode = cv2.erode(thresh, kernel, iterations = 1)
-    result = cv2.bitwise_or(gray, erode)
-    cv2.imshow("contrast",gray)
-    cv2.imshow("res",result)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-def BrightnessContrast(factor, image):
-    img = Image.fromarray(image)
-    enhancer = ImageEnhance.Contrast(img)
-    output = enhancer.enhance(factor)
-    return np.asarray(output)
-    
 def blur_and_threshold(gray):
     gray = cv2.GaussianBlur(gray, (3, 3), 2)
     threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     threshold = cv2.fastNlMeansDenoising(threshold, 11, 31, 9)
     return threshold
+
+
+# ## **Find the Biggest Contour**
+
+# **Note: We made sure the minimum contour is bigger than 1/10 size of the whole picture. This helps in removing very small contours (noise) from our dataset**
+
 
 def biggest_contour(contours, min_area):
     biggest = None
@@ -65,38 +41,81 @@ def biggest_contour(contours, min_area):
 
 
 def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
     pts = pts.reshape(4, 2)
     rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
 
+    # return the ordered coordinates
     return rect
 
+
+# ## Find the exact (x,y) coordinates of the biggest contour and crop it out
+
+
 def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
 
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
 
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype="float32")
 
+    # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
+    # return the warped image
     return warped
+
+
+# # Transformation the image
+
+# **1. Convert the image to grayscale**
+
+# **2. Remove noise and smoothen out the image by applying blurring and thresholding techniques**
+
+# **3. Use Canny Edge Detection to find the edges**
+
+# **4. Find the biggest contour and crop it out**
 
 
 def transformation(image):
@@ -105,43 +124,32 @@ def transformation(image):
     height, width, channels = image.shape
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image_size = gray.size
+
     threshold = blur_and_threshold(gray)
-    threshold = BrightnessContrast(2,threshold)
-    threshold = cv2.blur(threshold,(4,4))
     edges = cv2.Canny(threshold, 50, 150, apertureSize=7)
-
-    cv2.imshow("edges",edges)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     simplified_contours = []
 
     for cnt in contours:
         hull = cv2.convexHull(cnt)
-        simplified_contours.append(cv2.approxPolyDP(hull,0.001 * cv2.arcLength(hull, True), True))
+        simplified_contours.append(cv2.approxPolyDP(hull,
+                                                    0.001 * cv2.arcLength(hull, True), True))
     simplified_contours = np.array(simplified_contours,dtype=object)
-    
+
     biggest_n, approx_contour = biggest_contour(simplified_contours, image_size)
 
     threshold = cv2.drawContours(image, simplified_contours, biggest_n, (0, 255, 0), 1)
-    test = cv2.drawContours(image, contours, 0, (0, 255, 0), 1)
-    
-    cv2.imshow("cont",test)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
     dst = 0
     if approx_contour is not None and len(approx_contour) == 4:
          approx_contour = np.float32(approx_contour)
          dst = four_point_transform(threshold, approx_contour)
     croppedImage = dst
-    cleanedImage = final_image(croppedImage)
+    cleaned_image = final_image(croppedImage)
+    return cleaned_image
 
-    cv2.imshow("cont",croppedImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    return croppedImage
+
+# **Increase the brightness of the image by playing with the "V" value (from HSV)**
 
 def increase_brightness(img, value=30):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -154,14 +162,17 @@ def increase_brightness(img, value=30):
     return img
 
 def final_image(rotated):
+    # Create our shapening kernel, it must equal to one eventually
     kernel_sharpening = np.array([[0, -1, 0],
                                   [-1, 5, -1],
                                   [0, -1, 0]])
+    # applying the sharpening kernel to the input image & displaying it.
     sharpened = cv2.filter2D(rotated, -1, kernel_sharpening)
     sharpened = increase_brightness(sharpened, 30)
     return sharpened
 
 
-os.chdir(r"C:\Users\astro\Documents\Handwriting\Images")
-img = cv2.imread("image0.jpg", cv2.IMREAD_COLOR)
-annotate_image(img)
+image = cv2.imread(r"C:\Users\astro\Documents\Handwriting\Images\image0.jpg")
+blurred_threshold = transformation(image)
+
+cv2.imwrite(r"C:\Users\astro\Documents\Handwriting\Images\Final_Image.jpg", cleaned_image)
--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import kivy
 kivy.require('2.1.0') 
 from kivy.app import App
 from kivy.uix.label import Label
-import tensorflow as tf
-from tensorflow import keras
+#import tensorflow as tf
+#from tensorflow import keras
+from PIL import Image, ImageEnhance
 import re
 
 image = np.array([0])
@@ -27,7 +28,7 @@ class MyApp(App):
 
 def camera():
     global image
-    vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture(1)
     while True:
          ret, frame = vid.read()
          cv2.imshow("normal",frame)
@@ -111,70 +112,148 @@ def create_practice():
     annotate_image(image)
 
 def annotate_image(image):
-    lower_threshold = np.array([0,0,100])
-    upper_threshold = np.array([170,45,255])
-    lower_threshold2 = np.array([85,0,0])
-    upper_threshold2 = np.array([107,255,255])
     print("Generating Legibility Report")
-    scale_percent =  30# percent of original size
+    scale_percent =  100# percent of original size
     width = int(image.shape[1] * scale_percent / 100)
     height = int(image.shape[0] * scale_percent / 100)
     dim = (width, height)
     resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    cv2.imshow("original",resized)
 
-    hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_threshold, upper_threshold)
-    mask2 = cv2.inRange(hsv, lower_threshold2, upper_threshold2)
-    img = mask + mask2
+    cropped = transformation(resized)
+    width = cropped.shape[1]
+    height = cropped.shape[0]
+    cropped = cropped[5:(height-5)]
 
-    coord_array = generate_coordinates(img,mask2)
-    #for i in coord_array:
-        #x = int(i[2])
-        #y = int(i[4])
-        #cv2.circle(mask,(x,y),5,(0,255,0),2)
-
-    cv2.imshow("image",coord_array)
+    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+    gray = BrightnessContrast(3,gray)
+    ret,thresh = cv2.threshold(gray,165,255,cv2.THRESH_BINARY)
+    kernel = np.ones((1, 1), np.uint8)
+    erode = cv2.erode(thresh, kernel, iterations = 1)
+    result = cv2.bitwise_or(gray, erode)
+    cv2.imshow("contrast",gray)
+    cv2.imshow("res",result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def BrightnessContrast(factor, image):
+    img = Image.fromarray(image)
+    enhancer = ImageEnhance.Contrast(img)
+    output = enhancer.enhance(factor)
+    return np.asarray(output)
     
+def blur_and_threshold(gray):
+    gray = cv2.GaussianBlur(gray, (3, 3), 2)
+    threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    threshold = cv2.fastNlMeansDenoising(threshold, 11, 31, 9)
+    return threshold
 
-def generate_coordinates(image,mask):
-    image = image[:,120:(image.shape[1]-1)]
-    width = image.shape[1]
-    height = image.shape[0]
-    output = cv2.blur(image,(int(width/150),int(height/150)))
-    output = cv2.inRange(output,0,225)
-    lines = find_lines(mask)
-    for i in range(len(lines)-1):
-        sub_img = output[lines[i]:lines[i+1]]
-        cv2.imshow("image",sub_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        for j in range(width):
-            if np.sum(sub_img[:,j]) > 60:
-                print("1",end="")
-            else:
-                print("0",end="")
-        print("")
+def biggest_contour(contours, min_area):
+    biggest = None
+    max_area = 0
+    biggest_n = 0
+    approx_contour = None
+    for n, i in enumerate(contours):
+        area = cv2.contourArea(i)
 
-    return output
-def find_lines(image):
-    width = image.shape[1]
-    height = image.shape[0]
-    output = cv2.blur(image,(int(width/100),int(height/100)))
-    output = cv2.inRange(output,0,50)
-    lines = []
-    cur_line = False
-    iter = 1
-    for i in output:
-        line_likely = (np.sum(i) < 30000)
-        if(line_likely and not cur_line):
-            cur_line = True
-            lines.append(iter + (int(width/200)))
-        elif(not line_likely):
-            cur_line = False
-        iter+=1
-    return lines
+        if area > min_area / 10:
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+                biggest_n = n
+                approx_contour = approx
+
+    return biggest_n, approx_contour
+
+
+def order_points(pts):
+    pts = pts.reshape(4, 2)
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
+
+
+def transformation(image):
+    image = image.copy()
+
+    height, width, channels = image.shape
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_size = gray.size
+
+    threshold = blur_and_threshold(gray)
+    threshold = cv2.blur(threshold,(5,5))
+    edges = cv2.Canny(threshold, 50, 150, apertureSize=7)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    simplified_contours = []
+
+    for cnt in contours:
+        hull = cv2.convexHull(cnt)
+        simplified_contours.append(cv2.approxPolyDP(hull,0.001 * cv2.arcLength(hull, True), True))
+    simplified_contours = np.array(simplified_contours,dtype=object)
+
+    biggest_n, approx_contour = biggest_contour(simplified_contours, image_size)
+
+    threshold = cv2.drawContours(image, simplified_contours, biggest_n, (0, 255, 0), 1)
+
+    dst = 0
+    if approx_contour is not None and len(approx_contour) == 4:
+         approx_contour = np.float32(approx_contour)
+         dst = four_point_transform(threshold, approx_contour)
+    croppedImage = dst
+    cleaned_image = final_image(croppedImage)
+    return cleaned_image
+
+
+# **Increase the brightness of the image by playing with the "V" value (from HSV)**
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+def final_image(rotated):
+    kernel_sharpening = np.array([[0, -1, 0],
+                                  [-1, 5, -1],
+                                  [0, -1, 0]])
+    sharpened = cv2.filter2D(rotated, -1, kernel_sharpening)
+    sharpened = increase_brightness(sharpened, 30)
+    return sharpened
+
 
 def take_test():
     generate_practice(False)
